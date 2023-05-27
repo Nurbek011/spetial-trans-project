b7fe30baa24a51214837f85a2035ca90841bdd37
Nurbek011 <nurbekkuzievo7@gmail.com>
2023-05-27 18:25:01 +0500

    init:start is done

---
+++ src/layouts/FooterLayout/FooterLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="header">
+        <p>Footer</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/layouts/HeaderLayout/HeaderLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="header">
+        <p>Header</p>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
